74b3707b9e0a1813026616c5cdcb25fa8e3f2413
AK <70256065+Aarush-new@users.noreply.github.com>
2023-07-05 15:38:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var firebaseConfig = {
   apiKey: "AIzaSyDqnZKeeWb_5gI9EJPx1ycT3SRiLKN6H-Y",
   authDomain: "money-cd0d1.firebaseapp.com",
+  databaseURL: "https://money-cd0d1-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "money-cd0d1",
   storageBucket: "money-cd0d1.appspot.com",
   messagingSenderId: "19888883860",
@@ -19,9 +20,13 @@ const balanceElement = document.getElementById('balance');
 const amountInput = document.getElementById('amount');
 const transactionList = document.getElementById('transactions');
 
+let balance = 0.00; // Initialize balance
+let transactionHistory = []; // Initialize transaction history
+let promptCount = 3; // Number of prompts
+
 // Fetch data from Firebase on page load
 database.ref('balance').once('value', function(snapshot) {
-  balance = snapshot.val() || 0.00;
+  balance = parseFloat(snapshot.val()) || 0.00;
   updateDisplay();
 });
 
@@ -50,7 +55,7 @@ function debit() {
   const amount = parseFloat(amountInput.value);
   const description = document.getElementById('description').value;
   if (isNaN(amount) || amount <= 0) {
-    alert('Invalid amount!');
+    showCustomPrompt('Invalid amount!');
     return;
   }
 
@@ -70,7 +75,7 @@ function credit() {
   const amount = parseFloat(amountInput.value);
   const description = document.getElementById('description').value;
   if (isNaN(amount) || amount <= 0) {
-    alert('Invalid amount!');
+    showCustomPrompt('Invalid amount!');
     return;
   }
 
@@ -85,15 +90,70 @@ function credit() {
   database.ref('transactions').set(transactionHistory);
 }
 
+function showCustomPrompt(message) {
+  const promptContent = document.getElementById('promptContent');
+
+  promptContent.innerHTML = `
+    <p>${message}</p>
+    <button onclick="hidePrompt()" class="button-green">OK</button>
+  `;
+
+  const customPrompt = document.getElementById('customPrompt');
+  customPrompt.style.display = 'flex';
+}
+
 // Clear data
 function clearData() {
-  balance = 0.00;
-  transactionHistory = [];
-  updateDisplay();
+  if (promptCount === 3) {
+    showPrompt();
+  } else if (promptCount > 0) {
+    confirmClearData();
+  }
+}
 
-  // Save data to Firebase
-  database.ref('balance').set(balance);
-  database.ref('transactions').set(transactionHistory);
+// Show prompt
+function showPrompt() {
+  document.getElementById('customPrompt').style.display = 'flex';
+  document.getElementById('promptContent').innerHTML = `
+    <p>Are you sure? <span id="promptCount">${promptCount}</span></p>
+    <button onclick="confirmClearData()" class="button-red">Yes</button>
+    <button onclick="cancelPrompt()" class="button-green">Cancel</button>
+  `;
+}
+
+// Confirm clear data
+function confirmClearData() {
+  promptCount--;
+  if (promptCount > 0) {
+    document.getElementById('promptCount').textContent = promptCount;
+  } else {
+    balance = 0.00;
+    transactionHistory = [];
+    updateDisplay();
+
+    // Save data to Firebase
+    database.ref('balance').set(balance);
+    database.ref('transactions').set(transactionHistory);
+
+    hidePrompt();
+  }
+}
+
+// Cancel prompt
+function cancelPrompt() {
+  resetPromptCount();
+  hidePrompt();
+}
+
+// Hide prompt
+function hidePrompt() {
+  resetPromptCount();
+  document.getElementById('customPrompt').style.display = 'none';
+}
+
+// Reset prompt count
+function resetPromptCount() {
+  promptCount = 3;
 }
 
 // Initialize display
